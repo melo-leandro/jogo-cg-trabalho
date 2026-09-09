ef5f8ba9d7a546b44137f372226ffa5aa007d117
Leandro Melo <leandrocdmelo@gmail.com>
2026-09-09 20:51:55 -0300

    regula a medida das torres

--- T1/castle.js
+++ T1/castle.js
@@ -4,22 +4,22 @@ import {setDefaultMaterial} from "./libs/util/util.js";
 let castelo = new THREE.Group();
 
 let material = setDefaultMaterial();
-let towerGeometry = new THREE.CylinderGeometry(4, 4, 10);
+let towerGeometry = new THREE.CylinderGeometry(4.5, 4.5, 18);
 
 let towerNW = new THREE.Mesh(towerGeometry, material);
-towerNW.position.set(119.0, 2.0, -20.0);
+towerNW.position.set(119.0, 9.0, -20.0);
 castelo.add(towerNW);
 
 let towerSW = new THREE.Mesh(towerGeometry, material);
-towerSW.position.set(72.0, 2.0, -20.0);
+towerSW.position.set(72.0, 9.0, -20.0);
 castelo.add(towerSW);
 
 let towerNE = new THREE.Mesh(towerGeometry, material);
-towerNE.position.set(119.0, 2.0, 20.0);
+towerNE.position.set(119.0, 9.0, 20.0);
 castelo.add(towerNE);
 
 let towerSE = new THREE.Mesh(towerGeometry, material);
-towerSE.position.set(72.0, 2.0, 20.0);
+towerSE.position.set(72.0, 9.0, 20.0);
 castelo.add(towerSE);
 
 
