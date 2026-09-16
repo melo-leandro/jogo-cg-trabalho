164e7b6492c3d73c5339b8103c3a6bf527eb06ae
Yan Dias <yandiasferreira@gmail.com>
2026-09-16 01:21:54 -0300

    fix(bala): ajusta tamanho do projétil pra ficar proporcional ao cano da arma

---
+++ T1/tiro.js
@@ -0,0 +1,49 @@
+import * as THREE from 'three';
+import { setDefaultMaterial } from "./libs/util/util.js";
+
+const VEL_PROJETIL = 40;
+const DIST_MAX = 100;
+const CAD_MAX = 0.25;
+
+let projeteis = [];
+let ultimoTiro = -Infinity;
+let tempoDecorrido = 0;
+
+const materialProjetil = setDefaultMaterial("red");
+const geometriaProjetil = new THREE.SphereGeometry (0.08);
+
+export function atirar(scene, camera, ponta){
+    if (tempoDecorrido - ultimoTiro < CAD_MAX) return;
+    ultimoTiro = tempoDecorrido;
+
+    let direction = new THREE.Vector3();
+    camera.getWorldDirection(direction);
+
+    let armaPosition = new THREE.Vector3();
+    ponta.getWorldPosition(armaPosition);
+
+    const bala = new THREE.Mesh(geometriaProjetil, materialProjetil);
+    bala.position.copy(armaPosition);
+    scene.add(bala);
+
+    projeteis.push({ mesh: bala, direcao: direction, distanciaPercorrida: 0 });
+}
+
+export function atualizarProjeteis(delta, scene) {
+    tempoDecorrido += delta;
+
+    for (let i = projeteis.length - 1; i >= 0; i--) {
+        const p = projeteis[i];
+
+        const passo = VEL_PROJETIL * delta;
+
+        p.mesh.position.addScaledVector(p.direcao, passo);
+
+        p.distanciaPercorrida += passo;
+
+        if (p.distanciaPercorrida > DIST_MAX) {
+        scene.remove(p.mesh);
+        projeteis.splice(i, 1);
+        }
+    }
+}
